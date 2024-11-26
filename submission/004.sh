5f677d96eb778585d11e0e0bba0682350067775b
Caio Costa <costa.caiof@gmail.com>
2024-11-26 02:30:30 +0000

    solution for exercise 4

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+descriptor="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)"
+full_descriptor=$(bitcoin-cli getdescriptorinfo "$descriptor" | jq -r '.descriptor')
+tr_addr=$(bitcoin-cli deriveaddresses $full_descriptor | jq -r '.[0]')
+echo $tr_addr
